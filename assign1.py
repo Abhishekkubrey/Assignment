38a115b13722b18df6abdc81e19fb524a70f5ad7
Abhishekkubrey <abhishekkubrey95@gmail.com>
2021-08-08 20:54:56 +0530

    no change

---
+++ assign1.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the value of a:-"))
+b=int(input("Enter the value of b:-"))
+c=a+b
+c=print("a+b=",a+b)
+print(type(c),c)
+d=a-b
+d=print("a-b=",a-b)
+print(type(d),d)
+e=a*b
+e=print("a*b=",a*b)
+print(type(e),e)
+f=a/b
+f=print("a/b=",a/b)
+print(type(f),f)
+g=a**b
+g=print("a**b=",a**b)
+print(type(g),g)
